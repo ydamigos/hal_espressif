040fd995e0d6e7df4565b9180ac024f864e05a1f
Felipe Neves <felipe.neves@espressif.com>
2021-07-08 12:12:59 -0300

    hal: toolchain: override idf toolchain with zephyr
    This allows bootloader building without IDF toolchain
    
    Signed-off-by: Felipe Neves <felipe.neves@espressif.com>

--- tools/cmake/toolchain-esp32c3.cmake
+++ tools/cmake/toolchain-esp32c3.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Generic)
 
-set(CMAKE_C_COMPILER riscv32-esp-elf-gcc)
-set(CMAKE_CXX_COMPILER riscv32-esp-elf-g++)
-set(CMAKE_ASM_COMPILER riscv32-esp-elf-gcc)
+set(CMAKE_C_COMPILER ${ZEPHYR_CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER ${ZEPHYR_CMAKE_CXX_COMPILER})
+set(CMAKE_ASM_COMPILER ${ZEPHYR_CMAKE_ASM_COMPILER})
 
 set(CMAKE_C_FLAGS "-march=rv32imc" CACHE STRING "C Compiler Base Flags")
 set(CMAKE_CXX_FLAGS "-march=rv32imc" CACHE STRING "C++ Compiler Base Flags")
